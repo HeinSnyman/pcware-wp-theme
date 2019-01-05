f7188de88e951aa448e66895b27fa55d1eb373da
Hein Snyman <heinsnyman@Heins-MacBook-Pro.local>
2019-01-06 00:22:52 +0200

    Styled Single Product and added and styled cart page

--- inc/wc-hooks.php
+++ inc/wc-hooks.php
@@ -3,6 +3,9 @@
     add_action( 'woocommerce_single_product_summary', 'woocommerce_template_single_title', 1);
 	add_action( 'woocommerce_single_product_summary', 'woocommerce_template_single_price', 10 );
 
+    remove_action( 'woocommerce_after_single_product_summary', 'woocommerce_output_related_products', 20 );
+    remove_action( 'woocommerce_single_variation', 'woocommerce_single_variation', 10 );
     remove_action( 'woocommerce_single_product_summary', 'woocommerce_template_single_meta', 40 );
     remove_action( 'woocommerce_single_product_summary', 'woocommerce_template_single_add_to_cart', 30);
+
 ?>
