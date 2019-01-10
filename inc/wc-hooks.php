b112b8a99c37871f3b75b9702340f67169797833
Hein Snyman <heinsnyman@Heins-MacBook-Pro.local>
2019-01-10 21:42:13 +0200

    Configured Checkout Page

--- inc/wc-hooks.php
+++ inc/wc-hooks.php
@@ -6,4 +6,23 @@
     remove_action( 'woocommerce_single_product_summary', 'woocommerce_template_single_meta', 40 );
     remove_action( 'woocommerce_after_single_product_summary', 'woocommerce_output_related_products', 20 );
 
+
+
+    //CHECKOUT FILTER
+    add_filter( 'woocommerce_checkout_fields', 'checkout_field_classes' );
+
+    function checkout_field_classes( $fields ){
+        $fields['order']['order_comments']['type'] = 'textarea';
+        $fields['billing']['billing_address_2']['label'] = 'HELLLLO';
+
+        $fields['order']['order_comments'] = array(
+            'label'             =>      __('Order notes (optional)','woocommerce'),
+            'class'             =>      array('order-notes__field'),
+            'placeholder'       =>      _x('Special Order Notes','woocommerce')
+            
+        );
+        return $fields;
+    }
+
+
 ?>
